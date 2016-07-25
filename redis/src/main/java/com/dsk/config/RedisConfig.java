e877a13d9a8edc48e5bad156e36846d2b74d3d6e
dingshoukun <dingshoukun@wealink.com>
2016-07-26 00:05:03 +0800

    redis 序列化

--- redis/src/main/java/com/dsk/config/RedisConfig.java
+++ redis/src/main/java/com/dsk/config/RedisConfig.java
@@ -1,18 +1,23 @@
 package com.dsk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisAConfig {
+public class RedisConfig {
+
+
     @Bean
-    @Qualifier("RedisAConfig")
     public JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
         jedisConnectionFactory.setHostName("127.0.0.1");
+        jedisConnectionFactory.setPort(6379);
         return jedisConnectionFactory;
     }
 
@@ -20,7 +25,9 @@ public class RedisAConfig {
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
-
+        RedisSerializer<String> serializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(serializer);
+        redisTemplate.setValueSerializer(serializer);
         return redisTemplate;
     }
 }
--- redis/src/main/java/com/dsk/controller/RedisController.java
+++ redis/src/main/java/com/dsk/controller/RedisController.java
@@ -1,15 +1,15 @@
 package com.dsk.controller;
 
-import com.alibaba.fastjson.serializer.JSONSerializer;
-import com.dsk.config.RedisAConfig;
-import com.dsk.config.RedisBConfig;
-import com.mysql.jdbc.log.Log;
-import com.sun.tools.javac.util.List;
+import com.alibaba.fastjson.JSONObject;
+import com.dsk.config.RedisConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 测试类.
@@ -21,20 +21,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class RedisController {
 
     @Autowired
-    private RedisAConfig redisAConfig;
-
-    @Autowired
-    private RedisBConfig redisBConfig;
-
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
+    private RedisConfig redisAConfig;
 
     @RequestMapping("/redis")
     @ResponseBody
     public String redis(){
         redisAConfig.redisTemplate().opsForValue().set("aaa","111222");
-        return redisAConfig.redisTemplate().opsForValue().get("aaa");
-        //return stringRedisTemplate.opsForValue().get("aaa");
+        String value2 = redisAConfig.redisTemplate().opsForValue().get("aaa");;
+        Map data = new HashMap();
+        data.put("2",value2);
+        return JSONObject.toJSONString(data);
+
     }
 
 
